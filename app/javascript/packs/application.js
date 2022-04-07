1a13edae0cead35283eae2033e83c598c246bc05
Iti <iti.armpalu@gmail.com>
2022-04-07 14:21:14 +0400

    Fontawesome removed from packs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,7 +18,7 @@
 console.log('Hello World from Webpacker')
 
 import 'bootstrap/dist/css/bootstrap'
-import '@fortawesome/fontawesome-free/js/fontawesome'
-import '@fortawesome/fontawesome-free/js/solid'
-import '@fortawesome/fontawesome-free/js/regular'
-import '@fortawesome/fontawesome-free/js/brands'
+// // import '@fortawesome/fontawesome-free/js/fontawesome'
+// import '@fortawesome/fontawesome-free/js/solid'
+// import '@fortawesome/fontawesome-free/js/regular'
+// import '@fortawesome/fontawesome-free/js/brands'
